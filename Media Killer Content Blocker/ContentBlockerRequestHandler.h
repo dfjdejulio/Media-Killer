5b20bdf8062b4664562c0ed95dc5dd01bfceed9b
Doug DeJulio <ddj@cmu.edu>
2017-08-04 15:17:24 -0400

    Added "content blocker" extension.

---
+++ Media Killer Content Blocker/ContentBlockerRequestHandler.h
@@ -0,0 +1,13 @@
+//
+//  ContentBlockerRequestHandler.h
+//  Media Killer Content Blocker
+//
+//  Created by Doug DeJulio on 2017-08-04.
+//  Copyright © 2017 Doug DeJulio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContentBlockerRequestHandler : NSObject <NSExtensionRequestHandling>
+
+@end
---
+++ Media Killer Content Blocker/ContentBlockerRequestHandler.m
@@ -0,0 +1,26 @@
+//
+//  ContentBlockerRequestHandler.m
+//  Media Killer Content Blocker
+//
+//  Created by Doug DeJulio on 2017-08-04.
+//  Copyright © 2017 Doug DeJulio. All rights reserved.
+//
+
+#import "ContentBlockerRequestHandler.h"
+
+@interface ContentBlockerRequestHandler ()
+
+@end
+
+@implementation ContentBlockerRequestHandler
+
+- (void)beginRequestWithExtensionContext:(NSExtensionContext *)context {
+    NSItemProvider *attachment = [[NSItemProvider alloc] initWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"blockerList" withExtension:@"json"]];
+    
+    NSExtensionItem *item = [[NSExtensionItem alloc] init];
+    item.attachments = @[attachment];
+    
+    [context completeRequestReturningItems:@[item] completionHandler:nil];
+}
+
+@end
